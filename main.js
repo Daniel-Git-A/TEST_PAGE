a3388f64ece8a7bc1943bd5ad49bcb85b18ed2cd
Daniel-Git-A <62717985+Daniel-Git-A@users.noreply.github.com>
2026-02-15 13:23:17 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 const button = document.getElementById("coolBtn");
 
 
+const canvas = document.getElementById("renderCanvas");
+const engine = new BABYLON.Engine(canvas, true);
+
+const scene = new BABYLON.Scene(engine);
+const camera = new BABYLON.ArcRotateCamera(
+    "camera",
+    Math.PI / 2,
+    Math.PI / 2,
+    5,
+    BABYLON.Vector3.Zero(),
+    scene
+);
+camera.attachControl(canvas, true);
+
+const light = new BABYLON.HemisphericLight(
+    "light",
+    new BABYLON.Vector3(0, 1, 0),
+    scene
+);
+
+const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
+
+engine.runRenderLoop(() => {
+    scene.render();
+});
+
+
 var clickMe = function() {
     console.log("Button clicked!");
 }
