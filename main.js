31f9997b0a92997e0835b61c6a068dc63767a6de
Daniel-Git-A <62717985+Daniel-Git-A@users.noreply.github.com>
2026-02-16 11:26:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,52 @@
 const button = document.getElementById("coolBtn");
 
 
+// BABYLON SHIT START
 const canvas = document.getElementById("renderCanvas");
 const engine = new BABYLON.Engine(canvas, true);
 
-const scene = new BABYLON.Scene(engine);
-const camera = new BABYLON.ArcRotateCamera(
-    "camera",
-    Math.PI / 2,
-    Math.PI / 2,
-    5,
-    BABYLON.Vector3.Zero(),
-    scene
-);
-camera.attachControl(canvas, true);
+const createScene = async () => {
+    const scene = new BABYLON.Scene(engine);
 
-const light = new BABYLON.HemisphericLight(
-    "light",
-    new BABYLON.Vector3(0, 1, 0),
-    scene
-);
+    // Camera
+    const camera = new BABYLON.ArcRotateCamera(
+        "camera",
+        Math.PI / 2,
+        Math.PI / 2.5,
+        5,
+        BABYLON.Vector3.Zero(),
+        scene
+    );
+    camera.attachControl(canvas, true);
 
-const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
+    // Light
+    new BABYLON.HemisphericLight(
+        "light",
+        new BABYLON.Vector3(0, 1, 0),
+        scene
+    );
 
-engine.runRenderLoop(() => {
-    scene.render();
+    // Load GLB
+    await BABYLON.SceneLoader.AppendAsync(
+        "models/",      // folder
+        "tree.glb",    // file name
+        scene
+    );
+
+    return scene;
+};
+
+createScene().then(scene => {
+    engine.runRenderLoop(() => {
+        scene.render();
+    });
 });
 
+window.addEventListener("resize", () => {
+    engine.resize();
+});
+
+// BABYLON SHIT END
 
 var clickMe = function() {
     console.log("Button clicked!");
